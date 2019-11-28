f54c8e3784f003b37fa425982d83fb45b9bb699f
yuanrong.pan <yuanrong.pan@qizhidao.com>
2019-11-28 20:58:06 +0800

    first commit

---
+++ Example/Pods/Target Support Files/Pods-SWTComponentB_Tests/Pods-SWTComponentB_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SWTComponentB_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_SWTComponentB_Tests
+@end
---
+++ Example/Pods/Target Support Files/SWTComponentB/SWTComponentB-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SWTComponentB : NSObject
+@end
+@implementation PodsDummy_SWTComponentB
+@end
